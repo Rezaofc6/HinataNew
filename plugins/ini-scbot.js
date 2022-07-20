8fe74bbad36004eb3721734a5f2359fe8025221e
Rezaofc6 <108604209+Rezaofc6@users.noreply.github.com>
2022-07-20 12:16:39 +0700

    Update ini-scbot.js

--- plugins/ini-scbot.js
+++ plugins/ini-scbot.js
@@ -2,11 +2,12 @@ import fetch from 'node-fetch'
 let handler = async(m, { conn, text, usedPrefix, command }) => {
         let pp = await conn.profilePictureUrl(m.chat).catch(_ => null)
 let pepe = pp ? await (await fetch(pp)).buffer() : Buffer.alloc(0)
-let str = `*SAYA NYARI ESCE CUMA DI GITHUB BANH*\nSilahkan follow github saya dibawah ,Terimakasih`
-conn.sendHydrated(m.chat, str, wm, pepe, 'https://github.com/AyGemuy/', '𝙶𝙸𝚃𝙷𝚄𝙱', null, null, [
+let str = `*NYARI SC BANG? :V*\recode sendiri lah bg jgn main enak doang `
+conn.sendHydrated(m.chat, str, wm, pepe, 'https://instagram.com/itz.reza_official_', 'IG OWNER', null, null, [
 ['𝙼𝙴𝙽𝚄', '/menu']
 ], m)
 }
-handler.command = ['sc']
+handler.command = handler.help = ['sc']
+handler.tags = ['fun']
 
 export default handler
