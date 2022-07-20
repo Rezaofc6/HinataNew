7fbb1bcc959a0889af2f66d0431c686920c6c738
Rezaofc6 <108604209+Rezaofc6@users.noreply.github.com>
2022-07-20 11:54:41 +0700

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -5,15 +5,14 @@ let pepe = pp ? await (await fetch(pp)).buffer() : Buffer.alloc(0)
 let str = ` ${conn.getName(m.sender)} Want Support Bot?
 
 *PAYMENT ↓*
-_*Pulsa/pulse(Telkomsel):*_ ${pulsa}
+_*Pulsa(3):*_ ${pulsa}
 
 _*Dana/ovo:*_ ${dana}
-_*Paypal:*_ ${paypal}
-_*Saweria:*_ ${saweria}
-_*Trakteer:*_ ${trakteer}
+*DONASI DENGAN FOLLOW IG OWNER JUGA BOLEH*
+https://instagram.com/itz.reza_official_
 Setelah melakukan donasi kirim bukti pembayaran ke owner
 `
-conn.sendHydrated(m.chat, str, wm, pepe, 'https://github.com/AyGemuy/', '𝙶𝙸𝚃𝙷𝚄𝙱', null, null, [
+conn.sendHydrated(m.chat, str, wm, pepe, 'https://github.com/Rezaofc', '𝙶𝙸𝚃𝙷𝚄𝙱', null, null, [
 ['𝙼𝙴𝙽𝚄', '/menu']
 ], m)
 
