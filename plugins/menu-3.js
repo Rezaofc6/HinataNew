c1d9d0c9edb16f8c3b64b2871075003bdd8e5639
Rezaofc6 <108604209+Rezaofc6@users.noreply.github.com>
2022-07-21 02:09:10 +0700

    Update menu-3.js

--- plugins/menu-3.js
+++ plugins/menu-3.js
@@ -19,7 +19,7 @@ Hai, *%name!*
 *Fitur:* %totalfeatures command
 
 %readmore
-*Support me:* _https://s.id/Cerdasin62_
+*Support me:* https://instagram.com/itz.reza_official_
 *Note:*
 _Jika Respon Tidak Muncul Kemungkinan Terjadi Error_
 `.trimStart(),
