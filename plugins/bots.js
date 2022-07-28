e6cdb8e9960f74126b4cfa29129a404b74fc0247
Rezaofc6 <108604209+Rezaofc6@users.noreply.github.com>
2022-07-28 16:55:29 +0700

    Create bots.js

---
+++ plugins/bots.js
@@ -0,0 +1,53 @@
+import fs from 'fs'
+import fetch from 'node-fetch'
+let handler  = async (m, { conn, usedPrefix: _p }) => {
+let info = `*「 EzaBot Sudah Aktif 」*
+_SILAHKAN KETIK .menu UNTUK MENAMPILKAN MENU BOT_`
+
+let td = 'application/vnd.openxmlformats-officedocument.wordprocessingml.document'
+/*const message = {
+            document: { url: thumb },
+            jpegThumbnail: await (await fetch(thumb)).buffer(), fileName: global.wm, mimetype: td, fileLength: '9999999', pageCount: '999',
+            caption: info,
+            footer: wm,
+            templateButtons: [
+                {
+                    urlButton: {
+                        displayText: '🌎 OFFICIAL GROUP',
+                        url: sgc
+                    }
+                },
+                {
+                    callButton: {
+                        displayText: '📞 Add me',
+                        phoneNumber: nomorown
+                    }
+                },
+                {
+                    quickReplyButton: {
+                        displayText: 'MENU',
+                        id: '.menu'
+                    }
+                },
+                {
+                    quickReplyButton: {
+                        displayText: 'PING',
+                        id: '.ping'
+                    }
+                },
+                {
+                    quickReplyButton: {
+                        displayText: 'DONASI',
+                        id: '.donasi'
+                    }
+                },
+            ]
+        }
+        return await conn.sendMessage(m.chat, message)*/
+await conn.reply(m.chat, info, m, { quoted: fgif, contextInfo: { externalAdReply: { showAdAttribution: true, title: botdate, body: bottime, sourceUrl: snh, thumbnail: fs.readFileSync('./thumbnail.jpg') }}})
+
+}
+handler.customPrefix = /^(.bot|bot)$/i
+handler.command = new RegExp
+
+export default handler
