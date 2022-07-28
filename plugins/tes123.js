3626f929a05d0a59431bf8693ea46db424798bac
Rezaofc6 <108604209+Rezaofc6@users.noreply.github.com>
2022-07-28 19:35:06 +0700

    Update tes123.js

--- plugins/tes123.js
+++ plugins/tes123.js
@@ -1,7 +1,7 @@
 import fs from 'fs'
 import fetch from 'node-fetch'
 let handler  = async (m, { conn, usedPrefix: _p }) => {
-let info = `「 𝗕⦿𝗧 𝗦𝗨𝗗𝗔𝗛 𝗔𝗞𝗧𝗜𝗙 」`
+let info = `「 EZABOT 𝗦𝗨𝗗𝗔𝗛 𝗔𝗞𝗧𝗜𝗙 」`
 
 let td = 'application/vnd.openxmlformats-officedocument.wordprocessingml.document'
 /*const message = {
