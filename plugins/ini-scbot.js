d3ef2deb14c22bcdecfc4e7d26950551d24c1834
Rezaofc6 <108604209+Rezaofc6@users.noreply.github.com>
2022-07-30 19:21:37 +0700

    Update ini-scbot.js

--- plugins/ini-scbot.js
+++ plugins/ini-scbot.js
@@ -1,8 +1,10 @@
-import fetch from 'node-fetch'
+fetch from 'node-fetch'
 let handler = async(m, { conn, text, usedPrefix, command }) => {
         let pp = await conn.profilePictureUrl(m.chat).catch(_ => null)
 let pepe = pp ? await (await fetch(pp)).buffer() : Buffer.alloc(0)
-let str = `:(`
+let str = `*INI SCNYA*
+https://github.com/Rezaofc/test 
+*JANGAN LUPA FOOLOW GH RezaOfc`
 conn.sendHydrated(m.chat, str, wm, pepe, 'https://instagram.com/itz.reza_official_', 'IG OWNER', null, null, [
 ['𝙼𝙴𝙽𝚄', '/menu']
 ], m)
